e1fcfcc5e28fb14bcd8f7d11e5ea8a205576be9f
Mohamedbadhey <106878377+Mohamedbadhey@users.noreply.github.com>
2025-08-28 10:12:39 +0300

    cvbcvbncv

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "545530341"
+    serviceWorkerVersion: "674934330"
   }
 });
